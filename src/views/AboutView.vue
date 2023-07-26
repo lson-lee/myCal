3ed72867c988e6afd253056ed1589b2020c48c9f
lsonlee <lsonlee@hotmail.com>
2023-07-26 21:29:11 +0800

    feat: add calendar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>浅浅试一试一个追剧日历</h1>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <FullCalendar :options="calendarOptions"></FullCalendar>
+    <!-- <div>
+      <h2>设置每周开始日期</h2>
+      <span v-for="(el, index) in weekDayConfig" :key="el" @click="handleChangeStartDay(index)">{{ el }}</span>
+    </div> -->
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang="ts" setup>
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import zhCnLocale from '@fullcalendar/core/locales/zh-cn';
+import { computed, ref } from 'vue';
 
-@Options({
-  components: {
-    HelloWorld,
+const weekDayConfig = [
+    '周一',
+    '周二',
+    '周三',
+    '周四',
+    '周五',
+    '周六',
+    '周日',
+]
+const handleChangeStartDay = (index: number) => {
+  startDay.value = index;
+}
+
+let startDay = ref(0);
+let calendarOptions = {
+  plugins: [dayGridPlugin, interactionPlugin],
+  locales: [zhCnLocale],
+  locale: 'zh-cn',
+  initialView: 'dayGridMonth',
+  firstDay: startDay.value,
+  events: [{
+    title  : 'event1',
+    start  : '2023-08-01'
+  }],
+  dateClick: (obj: any) => {
+    console.log('dateClick', obj)
   },
-})
-export default class HomeView extends Vue {}
+  select: console.log,
+}
 </script>
